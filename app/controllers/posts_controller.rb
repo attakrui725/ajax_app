50a972d2eff6b20c2f93aed1ebcbf61c7fec78a6
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-11 12:24:02 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
     redirect_to action: :indesx
 
   end
+
+
+  def checked
+  post = Post.find(params[:id])
+  if post.checked
+    post.update(checked: false)
+  else
+    post.upade(checked: true)
+  end
+
+  item = Post.find(params[:id]
+  render json: { post: item })
+  end
+
 end
